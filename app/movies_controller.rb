3a3f9ee8525d34315763107341f95e198c5a2087
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-09-22 18:30:50 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -71,7 +71,7 @@ def can_be_found_updated_and_saved
   movie = Movie.create(title: "Awesome Flick")
   movie.update(title: "Even Awesomer Flick")
   movie.save
-  # __
+  movie
 end
 
 def can_update_using_update_method
